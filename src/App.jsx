01ea91012a05e2b273575b590c43fc6d8270164f
fliderman01 <fliderman01@gmail.com>
2025-09-20 16:48:35 +0400

    fix: make Header uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Products from "./pages/Products/Products";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 
 function App() {
   return (

64c98ec8a10a685ff5d4dcba85fcca61b98ef3ea
fliderman01 <fliderman01@gmail.com>
2025-09-22 14:04:13 +0400

    refactor: fetch avatar image from user data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./Header.css";
 import { Navbar, Container, Nav, Image } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Offcanvas } from "react-bootstrap";
 import CartSidebar from "./CartSidebar";
+import Cookies from "js-cookie";
 
 export default function Header() {
   const [show, setShow] = useState(false);
@@ -16,6 +17,10 @@ export default function Header() {
     navigate("/");
     location.reload();
   };
+
+  const userData = Cookies.get("user");
+  const user = userData && JSON.parse(userData);
+  const avatar = user && user.avatar ? user.avatar : "./images/avatar.png";
 
   return (
     <Navbar expand="lg" className="py-2">
@@ -33,24 +38,40 @@ export default function Header() {
         </Navbar.Brand>
 
         <Nav className="ms-auto d-flex align-items-center">
-          <Nav.Link href="#" className="p-0 me-3">
-            <Image
-              src="/images/cart.png"
-              alt="shopping cart"
-              className="shopping-cart"
-              width={24}
-              height={24}
-              onClick={handleShow}
-            />
-          </Nav.Link>
-          <Image
-            className="profile-img"
-            width={44}
-            height={44}
-            src="https://mn2s.com/wp-content/uploads/2024/11/Sunmi.png"
-            alt="Profile image"
-            roundedCircle
-          />
+          {user ? (
+            <>
+              <Image
+                src="/images/cart.png"
+                alt="shopping cart"
+                className="shopping-cart me-3"
+                style={{ cursor: "pointer" }}
+                width={24}
+                height={24}
+                onClick={handleShow}
+              />
+
+              <Image
+                className="profile-img"
+                width={44}
+                height={44}
+                src={avatar}
+                alt="Profile image"
+                roundedCircle
+              />
+            </>
+          ) : (
+            // TODO: add user symbol here
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "black",
+                // fontWeight: "SemiBold",
+              }}
+              to="/login"
+            >
+              Log in
+            </Link>
+          )}
         </Nav>
       </Container>
 

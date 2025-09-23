d13c4aab0eeb3876764d788c26fed29ab4ee7349
fliderman01 <fliderman01@gmail.com>
2025-09-23 22:14:33 +0400

    fix: incorrect success toas spacing

--- src/pages/Order/Success.jsx
+++ src/pages/Order/Success.jsx
@@ -15,12 +15,22 @@ export default function Success({ show, closeToast }) {
         width: 876,
         height: 590,
         zIndex: 11,
+        borderRadius: 0,
+        border: "none",
       }}
     >
-      <Toast.Header></Toast.Header>
+      <Toast.Header
+        style={{
+          border: "none",
+          display: "flex",
+          justifyContent: "end",
+          padding: "35px 40px 0 0 ",
+          fontSize: 23,
+        }}
+      />
       <div
         style={{
-          height: "100%",
+          marginTop: 50,
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
@@ -40,16 +50,25 @@ export default function Success({ show, closeToast }) {
             alt="success"
             style={{ width: 70, height: 70, objectFit: "cover" }}
           />
-          <h1 style={{ fontSize: 44, margin: "44px 0 20px 0" }}>Congrats!</h1>
+          <h1
+            style={{
+              fontSize: 42,
+              fontWeight: 600,
+              margin: "44px 0 25px 0",
+            }}
+          >
+            Congrats!
+          </h1>
           <p>Your order is placed successfully!</p>
         </div>
         <Link to={"/"}>
           <Button
             style={{
-              marginTop: 60,
+              marginTop: 58,
               fontSize: 14,
               padding: "10px 40px 10px 40px",
               borderRadius: 10,
+              backgroundColor: "#FF4000",
             }}
             variant="danger"
           >
